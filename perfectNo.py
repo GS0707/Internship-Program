3784a31758f7140bc825db3dbe0da13c4f0e02ee
Gajanan Shinde <68266624+GS0707@users.noreply.github.com>
2022-01-19 21:50:03 +0530

    Perfect Number upto N.

--- perfectNo.py
+++ perfectNo.py
@@ -1,4 +1,3 @@
-# Check how number are perfect number upto n.
 n=int(input("Enter the no. "))
 
 # check no. is grater than 6 or not
@@ -6,13 +5,14 @@ if n<6:
   print("Smallest perfect no is 6. Enter again valid no.")
   exit(0)
 
-# check each no. upto n
-for i in range(6,n):
-  sum=0
-  for j in range(1,i):
-    # if no. is divisible add it to sum
-    if i%j == 0:
-      sum+=j
-  #sum of divisors equal to that no. then it is perfect   
-  if sum == i:
-    print(i)
+def isPerfect(n):
+  # check each no. upto n
+  for i in range(6,n):
+    sum=0
+    for j in range(1,i):
+      # if no. is divisible add it to sum
+      if i%j == 0:
+        sum+=j
+    #sum of divisors equal to that no. then it is perfect   
+    if sum == i:
+      print(i)
